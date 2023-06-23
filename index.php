e6f71062bb29f06cb2fb6c331952f8a62332aafb
JulienDegermann <degermann.julien@gmail.com>
2023-06-23 13:57:48 +0200

    build page structure and home.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once 'templates/header.php' ?>
+
+<?php require_once 'pages/home.php' ?>
+
+
+
+<?php require_once 'templates/footer.php' ?>
---
+++ templates/footer.php
@@ -0,0 +1,38 @@
+<!----------------------------------------------------------------- MAIN END -->
+
+<!------------------------------------------------------------- FOOTER START -->
+<footer>
+  <div class="container">
+    <div class="row">
+
+      <div class="planning">
+        <h2>Horaires d'ouverture</h2>
+        <p>du lundi au vendredi : 8h00 - 12h00 / 14h00 - 18h00</p>
+        <p>samedi : 9h00 - 12h00 / 14h00 - 17h00</p>
+        <p>dimanche : // fermé //</p>
+      </div>
+
+      <div class="contact">
+        <a href="contact.php" class="button contact-button">
+          Contactez-nous
+          <a href="tel:0581234567" class="phone">05 81 23 45 67</a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--------------------------------------------------------------- FOOTER END -->
+
+
+<!------------------------------------------------------------ SCRIPTS CALLS -->
+<!-- jQuery -->
+<script src="assets/js/jquery_3.7.0.js"></script>
+<!-- Slick JS (sliders) -->
+<script type="text/javascript" src=" assets/js/slick-1.8.1/slick/slick.min.js"></script>
+<!--  -->
+<script src="assets/js/index.js"></script>
+<!-- page scpecific JS -->
+<script src="assets/js/<?php $current_page ?>.js"></script>
+
+</body>
+
+</html>
